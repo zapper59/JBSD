2c93f1074c64ae824400efbe2df8c642f15176b2
Justin Hill <jhillkwaj@gmail.com>
2015-09-11 22:40:14 -0500

    Can eat other spheres

---
+++ Game Jam 2015/Assets/Grow.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Grow : MonoBehaviour {
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.tag == "Player") {
+			if(this.transform.lossyScale.x * .9f > other.transform.lossyScale.x)
+			{
+				double thisVolume = (4/3)*Mathf.PI*Mathf.Pow(this.transform.lossyScale.x/2.0f,2);
+				double otherVolume = (4/3)*Mathf.PI*Mathf.Pow(other.transform.lossyScale.x/2.0f,2);
+				double newVolume = thisVolume + otherVolume;
+				float newRad = (float)Mathf.Pow((float)newVolume / Mathf.PI / 4 * 3, 1.0f/2.0f) * 2.0f;
+				this.transform.localScale = new Vector3(newRad, newRad, newRad);
+				Destroy(other.gameObject);
+			}
+		}
+	}
+}
