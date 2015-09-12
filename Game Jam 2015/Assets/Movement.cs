23d1167352eb88baf885bba8a2048129a337b0bc
Justin Hill <jhillkwaj@gmail.com>
2015-09-11 20:42:13 -0500

    Added alternate controls
    Can new use arrow keys

--- Game Jam 2015/Assets/Movement.cs
+++ Game Jam 2015/Assets/Movement.cs
@@ -6,19 +6,25 @@ public class Movement : MonoBehaviour {
 
 	
 	// Update is called once per frame
-	void FixedUpdate () {
-
-		if (Input.GetKey (KeyCode.W)) {
+	void Update () {
+		if (Input.GetKey (KeyCode.W) || Input.GetKey (KeyCode.UpArrow)) {
 			this.transform.Translate(this.transform.forward * Time.deltaTime);
 		}
-		if (Input.GetKey (KeyCode.A)) {
+		if (Input.GetKey (KeyCode.A)|| Input.GetKey (KeyCode.LeftArrow)) {
 			this.transform.Translate(-this.transform.right * Time.deltaTime);
 		}
-		if (Input.GetKey (KeyCode.S)) {
+		if (Input.GetKey (KeyCode.S)|| Input.GetKey (KeyCode.DownArrow)) {
 			this.transform.Translate(-this.transform.forward * Time.deltaTime);
 		}
-		if (Input.GetKey (KeyCode.D)) {
+		if (Input.GetKey (KeyCode.D)|| Input.GetKey (KeyCode.RightArrow)) {
 			this.transform.Translate(this.transform.right * Time.deltaTime);
+		}
+
+		if (Input.GetKey (KeyCode.Space) || Input.GetKey (KeyCode.LeftControl) || Input.GetKey (KeyCode.RightControl)) {
+			this.transform.Translate(-this.transform.up * Time.deltaTime);
+		}
+		if (Input.GetKey (KeyCode.LeftShift) || Input.GetKey (KeyCode.RightShift)) {
+			this.transform.Translate(this.transform.up * Time.deltaTime);
 		}
 	}
 }
