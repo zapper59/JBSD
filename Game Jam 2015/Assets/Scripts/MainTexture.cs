f7d032b8d275621deb9a4940d56c66b0d1df1701
Justin Hill <jhillkwaj@gmail.com>
2015-09-12 11:13:36 -0500

    New Textures

---
+++ Game Jam 2015/Assets/Scripts/MainTexture.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainTexture : MonoBehaviour {
+	public Texture[] textures;
+	MeshRenderer rend = null;
+	float scrollSpeed = .01f;
+	// Use this for initialization
+	void Start () {
+		int num = Random.Range(0,textures.Length);
+		rend = this.GetComponent<MeshRenderer>();
+		rend.material.mainTexture = textures[num];
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		float offset = Time.time * scrollSpeed;
+		rend.material.SetTextureOffset("_MainTex", new Vector2(offset, 0));
+	}
+}
