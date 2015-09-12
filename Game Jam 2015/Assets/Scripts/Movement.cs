1213625499d96085ee3705b7461a6fc2e0ba40fc
Justin Hill <jhillkwaj@gmail.com>
2015-09-12 09:43:24 -0500

    Redid movement code and added controller support

--- Game Jam 2015/Assets/Scripts/Movement.cs
+++ Game Jam 2015/Assets/Scripts/Movement.cs
@@ -15,12 +15,12 @@ public class Movement : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         //zoomCamera();
-		if (Input.GetKeyDown ("escape")) {
+		if (Input.GetKeyDown (KeyCode.Escape)) {
 			Screen.lockCursor = false;
 			Cursor.visible = true;
 		}
 		float moveAmount = ((moveSpeed / this.transform.lossyScale.x) + moveSpeed)/2.0f;
-		if (Input.GetKey (KeyCode.W) || Input.GetKey (KeyCode.UpArrow)) {
+		/*if (Input.GetKey (KeyCode.W) || Input.GetKey (KeyCode.UpArrow)) {
 			this.transform.Translate(moveAmount * new Vector3(0,0,1) * Time.deltaTime);
 		}
 		if (Input.GetKey (KeyCode.A)|| Input.GetKey (KeyCode.LeftArrow)) {
@@ -31,7 +31,7 @@ public class Movement : MonoBehaviour {
 		}
 		if (Input.GetKey (KeyCode.D)|| Input.GetKey (KeyCode.RightArrow)) {
 			this.transform.Translate(moveAmount * new Vector3(1,0,0) * Time.deltaTime);
-		}
+		}*/
 		
 		if (Input.GetKey (KeyCode.Space) || Input.GetKey (KeyCode.LeftControl) || Input.GetKey (KeyCode.RightControl)) {
 			this.transform.Translate(moveAmount * new Vector3(0,-1,0) *  Time.deltaTime);
@@ -39,37 +39,26 @@ public class Movement : MonoBehaviour {
 		if (Input.GetKey (KeyCode.LeftShift) || Input.GetKey (KeyCode.RightShift)) {
 			this.transform.Translate(moveAmount * new Vector3(0,1,0) *  Time.deltaTime);
 		}
-		
-		//The old control method
-		/*if (Input.mousePosition.x < Screen.width / 4.0 ) {
-			this.transform.RotateAround(this.transform.position, this.transform.up,-20f * Time.deltaTime);
-		}else if (Input.mousePosition.x > Screen.width * 3 / 4.0 ) {
-			this.transform.RotateAround(this.transform.position, this.transform.up,20f * Time.deltaTime);
+
+		if (Input.GetAxis ("Horizontal") != 0) {
+			this.transform.Translate(moveAmount * new Vector3(1,0,0) * Time.deltaTime * Input.GetAxis("Horizontal"));
 		}
 
-		if (Input.mousePosition.y < Screen.height / 4.0 ) {
-			this.transform.RotateAround(this.transform.position, this.transform.right,20f * Time.deltaTime);
-		}else if (Input.mousePosition.y > Screen.height * 3 / 4.0 ) {
-			this.transform.RotateAround(this.transform.position, this.transform.right,-20f * Time.deltaTime);
-		}*/
-		
+		if (Input.GetAxis ("Vertical") != 0) {
+			this.transform.Translate(moveAmount * new Vector3(0,0,1) * Time.deltaTime * Input.GetAxis("Vertical"));
+		}
+
 		if (Input.GetAxis ("Mouse X") > .2 || Input.GetAxis ("Mouse X") < -.2) {
 			this.transform.RotateAround(this.transform.position, this.transform.up,mouseSpeed * Time.deltaTime * Input.GetAxis("Mouse X"));
+		}else if (Mathf.Abs(Input.GetAxis ("HorizontalY")) >  .1f) {
+			this.transform.RotateAround(this.transform.position, this.transform.up,mouseSpeed * Time.deltaTime * Input.GetAxis("HorizontalY"));
 		}
-		
+
 		if (Input.GetAxis ("Mouse Y") > .2 || Input.GetAxis ("Mouse Y") < -.2) {
-			this.transform.RotateAround(this.transform.position, this.transform.right,-mouseSpeed * Time.deltaTime * Input.GetAxis("Mouse Y"));
+			this.transform.RotateAround(this.transform.position, this.transform.right,mouseSpeed * Time.deltaTime * Input.GetAxis("Mouse Y"));
+		}else if (Mathf.Abs(Input.GetAxis ("VerticalY")) > .1f) {
+			this.transform.RotateAround(this.transform.position, this.transform.right,mouseSpeed * Time.deltaTime * Input.GetAxis("VerticalY"));
 		}
 	}
-
-   /* float smooth = .05f;
-    float cameraZ = 5;
-    void zoomCamera () {
-        //Camera.main.transform.localPosition = new Vector3(0, 0, Mathf.SmoothDamp(
-               // Camera.main.transform.position.z, cameraZ, smooth));
-        //Camera.main.fieldOfView = Mathf.Lerp(Camera.main.fieldOfView, 150, smooth);
-        print(Camera.main.transform.localPosition);
-
-    }*/
 }
 
