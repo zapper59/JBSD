47327b65c7754f650f9f119b22f643e73fe09cc7
Justin Hill <jhillkwaj@gmail.com>
2015-09-11 20:06:23 -0500

    Movement with W key
    Can now move forward

---
+++ Game Jam 2015/Assets/Movement.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+
+
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.W)) {
+			this.transform.Translate(this.transform.forward);
+		}
+	}
+}
