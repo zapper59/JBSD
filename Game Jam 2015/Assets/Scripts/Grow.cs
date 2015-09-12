6a4757a82ecd4a5c4146557b8df60dc75677abc0
Brian Richer <bririche@cs.utexas.edu>
2015-09-12 18:28:07 -0500

    Smooth camera resize/ faster grow/ better music

--- Game Jam 2015/Assets/Scripts/Grow.cs
+++ Game Jam 2015/Assets/Scripts/Grow.cs
@@ -9,7 +9,8 @@ public class Grow : MonoBehaviour {
 				double thisVolume = (4.0f/3.0f)*Mathf.PI*Mathf.Pow(this.transform.lossyScale.x/2.0f,2);
 				double otherVolume = (4.0f/3.0f)*Mathf.PI*Mathf.Pow(other.transform.lossyScale.x/2.0f,2);
 				double newVolume = thisVolume + otherVolume;
-				float newRad = (float)Mathf.Pow((float)newVolume / Mathf.PI / 4 * 3, 1.0f/2.0f) * 2.0f;
+				float newRad = (float)Mathf.Pow((float)newVolume / Mathf.PI / 4 * 3, 1.0f/2.0f) * 2.2f;
+                print(newRad);
 				this.transform.localScale = new Vector3(newRad, newRad, newRad);
 				Destroy(other.gameObject);
 			}
--- Game Jam 2015/Assets/Scripts/ResizeCamera.cs
+++ Game Jam 2015/Assets/Scripts/ResizeCamera.cs
@@ -4,13 +4,18 @@ using System.Collections;
 public class ResizeCamera : MonoBehaviour {
 	public GameObject mainCamera;
 	double nextLevelUp = 2;
+    float z = -2.5f;
+
 	
 	// Update is called once per frame
 	void Update () {
-		if(this.transform.localScale.x >= nextLevelUp)
-		{
-			mainCamera.transform.localPosition = new Vector3(0,0,mainCamera.transform.localPosition.z*2);
-			nextLevelUp *= 2;
-		}
+
+        if (this.transform.localScale.x >= nextLevelUp)
+        {
+            z *= 2;
+            nextLevelUp *= 2;
+        }
+
+		mainCamera.transform.localPosition = new Vector3(0,0,Mathf.Lerp(mainCamera.transform.localPosition.z, z, .1f));
 	}
 }
