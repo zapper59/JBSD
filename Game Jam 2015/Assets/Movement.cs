d6cbc5d115c725d404024f1c871c891102813613
Justin Hill <jhillkwaj@gmail.com>
2015-09-11 20:20:03 -0500

    Added movement in 2D

--- Game Jam 2015/Assets/Movement.cs
+++ Game Jam 2015/Assets/Movement.cs
@@ -6,9 +6,19 @@ public class Movement : MonoBehaviour {
 
 	
 	// Update is called once per frame
-	void Update () {
+	void FixedUpdate () {
+
 		if (Input.GetKey (KeyCode.W)) {
-			this.transform.Translate(this.transform.forward);
+			this.transform.Translate(this.transform.forward * Time.deltaTime);
+		}
+		if (Input.GetKey (KeyCode.A)) {
+			this.transform.Translate(-this.transform.right * Time.deltaTime);
+		}
+		if (Input.GetKey (KeyCode.S)) {
+			this.transform.Translate(-this.transform.forward * Time.deltaTime);
+		}
+		if (Input.GetKey (KeyCode.D)) {
+			this.transform.Translate(this.transform.right * Time.deltaTime);
 		}
 	}
 }
